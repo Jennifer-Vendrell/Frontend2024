392e1f37947eee6613aac40b74cdad2298a94726
Jennifer Vendrell Javier <isc20350326@gmail.com>
2024-04-25 11:07:59 -0600

    localstorage

--- ToDoList/src/App.jsx
+++ ToDoList/src/App.jsx
@@ -1,54 +1,34 @@
-import List from "./List"
-import TaskModal from "./TaskModal"
-
-const taskList = [
-  {
-  id: 1,
-  task: "Estudiar",
-  description:"Estudiar para el examen de calculo",
-  limit: "13:00",
-  location:"Escuela",
-  isDone: false,
-},
-{
-  id: 2,
-  task: "Practicar",
-  description:"Entrar para el siguiente partido",
-  limit: "14:00",
-  location:"Campo",
-  isDone: false,
-},
-{
-  id: 3,
-  task: "Trabajar",
-  description:"Hacer las tareas de la escuela",
-  limit: "15:00",
-  location:"Casa",
-  isDone: false,
-}
-]
-
-function App() {
-
-  return (
-    <div className="container">
-      <h1>To Do List</h1>
-      <hr />
-      <List 
-      taskList={taskList}
-      />
-      <hr />
-      <div className="text-end">
-        <TaskModal/>
-        <button
-        type="button"
-        className="btn btn-outline-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#taskModal"
-        ><i className="bi bi-plus-lg"></i>Add</button>  
-      </div>
-    </div>
-  )
-}
-
+import { useState } from "react" 
+import List from "./List" 
+import TaskModal from "./TaskModal" 
+ 
+function App() { 
+ 
+  const [taskList, setTaskList] = useState( 
+    JSON.parse(localStorage.getItem("taskList")) || [] 
+    ) 
+   
+    localStorage.setItem("taskList", JSON.stringify(taskList))  
+  
+  return ( 
+     
+      <div className="container"> 
+          <h1>To Do List</h1> 
+          <hr /> 
+          <List  
+      taskList={taskList} 
+      /> 
+      <hr /> 
+      <div className="text-end"> 
+      <TaskModal taskList={taskList}  
+      setTaskList={setTaskList}/> 
+      <button type="button" className="btn btn-primary"  
+      data-bs-toggle="modal" 
+      data-bs-target="#TaskModal"> 
+        <i className="bi bi-plus-lg"></i>Add</button> 
+      </div> 
+    </div> 
+  ) 
+} 
+ 
 export default App
--- ToDoList/src/TaskModal.jsx
+++ ToDoList/src/TaskModal.jsx
@@ -1,108 +1,123 @@
-import { useForm } from "./Hooks/UseForm"
+import { useForm } from "./Hooks/UseForm" 
+ 
+const taskInfo = { 
+    task: "", 
+    description: "", 
+    limit: "", 
+    location: "", 
+} 
+ 
+const TaskModal = ({ taskList,setTaskList }) => { 
+    const [values, handleInputChange, reset] = useForm(taskInfo) 
+ 
+    const handleSaveClick = () => { 
+        setTaskList([ 
+            ...taskList, 
+            { 
+                id: taskList.length + 1, 
+                ...values, 
+                isDone: false 
+            } 
+        ]) 
+ 
+        localStorage.setItem("taskList", JSON.stringify(taskList)) 
+        reset() 
+    } 
+ 
+    return ( 
+        <div className="modal fade" id={"TaskModal"}> 
+            <div className="modal-dialog modal-dialog-centered"> 
+                <div className="modal-content"> 
+                    <div className="modal-header"> 
+                        <h5 className="modal-title" 
+                            id="taskModalLabel"> 
+                            Add new Task 
+                        </h5> 
+ 
+                    </div> 
+                    <div className="modal-body"> 
+                        <div className="row"> 
+                            <div className="col text-start"> 
+                                <label 
+                                    className="form-label" 
+                                    htmlFor="task" 
+                                > 
+                                    Task 
+                                </label> 
+ 
+                                <input className="form-control" 
+                                    id="task" 
+                                    name="task" 
+                                    onChange={handleInputChange} 
+                                    type="text" 
+                                    value={values.task} 
+                                /> 
+ 
+                                <label 
+                                    className="form-label" 
+                                    htmlFor="description"> 
+                                    Description 
+                                </label> 
+ 
+                                <textarea 
+                                    className="form-control" 
+                                    id="description" 
+                                    name="description" 
+                                    onChange={handleInputChange} 
+                                    value={values.description}></textarea> 
+ 
+ 
+                                <label 
+                                    className="form-label"> 
+                                    Location 
+                                </label> 
+                                <input className="form-control" 
+                                    id="location" 
+                                    name="location" 
+                                    onChange={handleInputChange} 
+                                    value={values.location} 
+                                    type="text" 
+                                /> 
+ 
+                                <label className="form-label" 
+                                    htmlFor="limit" 
+                                > 
+                                    Limit 
+                                </label> 
+ 
+                                <input className="form-control" 
+                                    id="limit" 
+                                    name="limit" 
+                                    onChange={handleInputChange} 
+                                    type="time" 
+                                    value={values.limit} 
+                                /> 
+                            </div> 
+                        </div> 
+ 
+                    </div> 
+                    <div className="modal-footer"> 
+ 
+                        <button className="btn btn-sm btn-primary" 
+                        onClick={handleSaveClick}> 
+                            <i className="bi bi-pencil-square"></i> 
+                            Save 
+                        </button> 
+                        <button
 
-const taskInfo={
-    task:"",
-    description:"",
-    location:"",
-    limit:"",
-}
-
-const TaskModal = () => {
-    const [values, handleInputChange, reset]= useForm(taskInfo)
-
-    return(
-        <div className="modal fade" id={"taskModal"}>
-            <div className="modal-dialog mdoal-dialog-centered">
-                <div className="modal-content">
-            <div className="modal-header">
-                    <h5 
-                    className="modal-title"
-                    id="taskModalLabel"
-                    >
-                        Add New Task
-                    </h5>
-                </div>
-                <div className="modal-body">
-                    <div className="row">
-                        <div className="col">
-                         <label 
-                         className="form-label"
-                         htmlFor="task"
-                         >Task</label>
-
-                         <input
-                         className="form-control"
-                         id="task"
-                         name="task"
-                         onChange={handleInputChange}
-                         type="text"
-                         value={values.task}
-                         />
-
-                         <label
-                         className="form-label"
-                         htmlFor="description"
-                         >Description</label>
-
-                         <textarea
-                         className="form-control"
-                         id="description"
-                         name="description"
-                         onChange={handleInputChange}
-                         value={values.description}
-                         ></textarea>
-
-                         
-                         <label
-                         className="form-label"
-                         htmlFor="location"
-                         >location</label>
-
-                         <input
-                         className="form-control"
-                         id="location"
-                         name="location"
-                         type="text"
-                         
-                         />
-
-                         <label
-                         className="form-label"
-                         htmlFor="limit"
-                         >Limit</label>
-
-                         <input
-                         className="form-control"
-                         id="limit"
-                         name="limit"
-                         type="time"
-                         onChange={handleInputChange}
-                         value={values.description}
-                         />
-                        
-                        </div>
-                    </div>
-                </div>
-                <div className="modal-footer">
-                    <button className="btn btn-sm btn-outline-primary"
-                    onClick={() => console.log(values)}
-                    >
-                    <i className="bi bi-pencil-square"></i> 
-                    Save
-                    </button>
-                    <button
-                    type="button"
-                    className="btn btn-outline-secondary"
-                    data-bs-dismiss="modal"
-                    >
-                    <i className="bi bi-x-lg"></i>
-                        Close
-                    </button>
-                </div>
-                </div>
-            </div>
-        </div>
-    )
-}
-
+className="btn btn-sm btn-outline-secondary" data-bs-dismiss="modal"> 
+                            <i className="bi bi-x -lg"></i> 
+                            Close 
+                        </button> 
+ 
+ 
+ 
+                    </div> 
+                </div> 
+            </div> 
+        </div> 
+    ) 
+ 
+} 
+ 
 export default TaskModal
